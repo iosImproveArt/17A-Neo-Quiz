cb08ac28f7354db3c503ff5e5164fb88fb0336a2
iosImproveArt <impiosart@gmail.com>
2024-11-22 18:05:21 +0200

    init

---
+++ Neo-Quiz/Neo_QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  Neo_QuizApp.swift
+//  Neo-Quiz
+//
+//  Created by Improve on 22.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct Neo_QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
